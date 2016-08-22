a001f9c506eef3d7a03660439a3698090f3aacf9
Masaki Isono <isonoetiru@gmail.com>
2016-08-22 14:13:38 +0900

    1st commit

---
+++ src/resource.js
@@ -0,0 +1,31 @@
+var res = {
+    background_png : "res/background.png",
+    background2_png : "res/rock_above.png",
+    background3_png : "res/rock_under.png",
+    background4_png : "res/ceiling.png",
+    background5_png : "res/land.png",
+    ship_png : "res/shrimp02.png",
+    particle_png : "res/wave.png",
+    nagoya0_png : "res/nagoya0.png",
+    nagoya1_png : "res/nagoya1.png",
+    nagoya2_png : "res/nagoya2.png",
+    nagoya3_png : "res/nagoya3.png",
+    nagoya4_png : "res/nagoya4.png",
+    nagoya5_png : "res/nagoya5.png",
+    nagoya6_png : "res/nagoya6.png",
+    title_png : "res/Title.png",
+    start_png : "res/start.png",
+    gameover_png : "res/gameover.png",
+    replay_button_png : "res/replay_button.png",
+    coral_above_png : "res/coral_above.png",
+    coral_under_png : "res/coral_under.png",
+    bgm_main : "res/bgm_main.wav",
+    se_bang: "res/damage01.wav",
+    se_death: "res/Death.mp3",
+    se_decide: "res/se_decide.mp3"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
---
+++ src/result.js
@@ -0,0 +1,52 @@
+//result
+var ResultLayer = cc.Layer.extend({
+    ctor: function() {
+        this._super();
+        var size = cc.director.getWinSize();
+        // 画像の追加
+        var sprite = cc.Sprite.create(res.gameover_png);
+        sprite.setPosition(size.width / 2, size.height * 5 / 6);
+        sprite.setScale(0.8);
+        this.addChild(sprite, 0);
+
+        var sprite = cc.Sprite.create(res.replay_button_png);
+        sprite.setPosition(size.width / 2, size.height / 6);
+        sprite.setScale(0.8);
+        this.addChild(sprite, 1);
+
+        var label = cc.LabelTTF.create("Score:" + score, "Arial", 32);
+   label.setPosition(size.width / 2, size.height  / 2);
+   this.addChild(label, 1);
+
+
+        // タップイベントリスナーを登録する
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            onTouchBegan: this.onTouchBegan,
+            onTouchMoved: this.onTouchMoved,
+            onTouchEnded: this.onTouchEnded
+        }, this);
+        return true;
+    },
+    onTouchBegan: function(touch, event) {
+        return true;
+    },
+    onTouchMoved: function(touch, event) {},
+    onTouchEnded: function(touch, event) {
+        cc.director.runScene(new gameScene());
+    },
+});
+var ResultScene = cc.Scene.extend({
+    onEnter: function() {
+        this._super();
+        miss = 0;
+        // 背景レイヤーをその場で作る
+        var backgroundLayer = new cc.LayerColor(new cc.Color(255, 255, 255, 128));
+        this.addChild(backgroundLayer);
+        //ラベルとタップイベント取得
+        var layer3 = new ResultLayer();
+        this.addChild(layer3);
+
+    }
+});
